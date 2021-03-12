3698b62cef2e577bc72612e170361d2ab32af0f2
juancarlosSH <juancasu_900@hotmail.com>
2021-03-12 00:11:00 -0600

    home page update

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
   <v-app>
-    <Header></Header>
+    <v-app-bar app flat color="blue darken-2">
+      <v-img lazy-src="@/assets/icono.svg" max-height="52" max-width="52" src="@/assets/icono.svg"
+      ></v-img>
+      <v-spacer></v-spacer>
+      <v-btn v-for="(link) in links" :key="link" :to="{name: link.route}" text color="white"
+      >{{link.name}}</v-btn>
+      <v-spacer></v-spacer>
+      <!--<v-btn :to="{name: 'Login'}" color="white" outlined rounded
+      >Acceso Administrador</v-btn>-->
+    </v-app-bar>
+
     <v-main>
-      <router-view></router-view>
+      <v-container fluid class="pa-0">
+        <router-view></router-view>
+      </v-container>
     </v-main>
-    <Footer></Footer>
+
+    <v-footer app absolute color="blue darken-2">
+      <v-card-text class="white--text pt-0">
+        © 2020 Universidad Veracruzana. Todos los derechos reservados.
+      </v-card-text>
+      <v-card-text class="white--text pt-0">
+        {{ new Date().getFullYear() }} — Dirección: Lomas del Estadio s/n C.P. 91000 Xalapa, Veracruz, México
+      </v-card-text>
+    </v-footer>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import Header from './components/Header.vue';
-import Footer from './components/Footer.vue';
 
 export default Vue.extend({
   name: 'App',
 
-  components: {
-    Header,
-    Footer,
-  },
-
   data: () => ({
-
+    links: [
+      {name: 'Inicio', route: 'Home'},
+      {name: 'Observaciones', route: 'Observations'},
+      //{name: 'Estadistícas', route: 'Statistics'},
+      {name: 'Descargas', route: 'Downloads'},
+      //{name: 'Contacto', route: 'Contact'},
+    ],
   }),
 });
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,38 +1,30 @@
 <template>
-  <v-container fluid>
-    <v-card flat color="green lighten-4">
-      <v-row>
-        <v-col cols="8">
-          <div class="text-md-h3">
-            ¡Bienvenido a Veme Obeme!
-          </div>
-          <v-card-text class="text--primary">
-            Aquí encontraras el registro de las observaciones realizadas por
+  <v-card flat color="green lighten-4">
+    <v-row>
+      <v-col cols="8">
+        <v-card-title>
+          <h1 class="font-weight-regular">¡Bienvenido a Veme Obeme!</h1>
+        </v-card-title>
+        <v-card-text class="text-justify font-weight-regular pa-4 text-md-h6">
+          <p>Aquí encontraras el registro de las observaciones realizadas por
             internos, pasantes y residentes de medicina, sobre el proceso educativo
             en las unidades de salud en México. Si cursas el internado médico, el
             servicio social o la residencia y deseas opinar sobre las fortalezas y
             oportunidades de la educación médica en las unidades de salud de México,
-            te invitamos a colaborar con nosotros a través de la aplicación móvil.
-          </v-card-text>
-          <v-card-actions>
-            <v-btn
-                :to="{name: 'Downloads'}"
-                color="green lighten-2"
-            >Descarga la aplicación móvil</v-btn>
-          </v-card-actions>
-        </v-col>
-        <v-col cols="4">
+            te invitamos a colaborar con nosotros a través de la aplicación móvil.</p>
+        </v-card-text>
+        <v-card-actions class="px-5">
+          <v-btn :to="{name: 'Downloads'}" color="green lighten-2"
+          >Descarga la aplicación móvil</v-btn>
+        </v-card-actions>
+      </v-col>
+      <v-col cols="4" class="d-flex flex-row-reverse">
           <v-img
-              lazy-src="../assets/InicioCel.png"
-              max-height="400"
-              max-width="400"
-              src="../assets/InicioCel.png"
+            lazy-src="../assets/InicioCel.png" max-height="400" max-width="400" src="../assets/InicioCel.png"
           ></v-img>
         </v-col>
-      </v-row>
-    </v-card>
-    <Welcome></Welcome>
-  </v-container>
+    </v-row>
+  </v-card>
 </template>
 
 <script lang="ts">
