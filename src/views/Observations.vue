71728e3907417b17e96d5b3c45984e5aa394d64a
juancarlosSH <juancasu_900@hotmail.com>
2021-03-30 10:15:36 -0600

    Download observations in CSV file

--- src/views/Observations.vue
+++ src/views/Observations.vue
@@ -5,7 +5,7 @@
         <h2 class="font-weight-medium text-md-h4 pa-5">Observaciones</h2>
       </v-col>
       <v-col cols="4" class="d-flex justify-end align-center pr-10">
-        <v-btn color="green lighten-1"
+        <v-btn color="green lighten-1" @click="download_csv"
           >CSV
           <v-img
             lazy-src="@/assets/download.png"
@@ -36,6 +36,7 @@
 
 <script lang="ts">
 import Vue from "vue";
+import { mapState } from "vuex";
 import ObservationsHeader from "@/components/ObservationsHeader.vue";
 import ObservationsInformation from "@/components/ObservationsInformation.vue";
 import ObservationsTable from "@/components/ObservationsTable.vue";
@@ -46,6 +47,34 @@ export default Vue.extend({
   components: {
     ObservationsHeader,
     ObservationsTable,
+  },
+
+  data() {
+    return {};
+  },
+
+  methods: {
+    download_csv: function() {
+      var csv = "";
+      this.observations.forEach(function(row: any) {
+        csv += row.healthInstitution + ",";
+        csv += row.registrationDate + ",";
+        csv += row.observationDate + ",";
+        csv += row.observationTime + ",";
+        csv += row.typeObservation + ",";
+        csv += row.typeStudent + ",";
+        csv += "\n";
+      });
+      var hiddenElement = document.createElement("a");
+      hiddenElement.href = "data:text/csv;charset=utf-8," + encodeURI(csv);
+      hiddenElement.target = "_blank";
+      hiddenElement.download = "observations.csv";
+      hiddenElement.click();
+    },
+  },
+
+  computed: {
+    ...mapState(["observations"]),
   },
 });
 </script>
